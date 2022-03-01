0893bffa0f211da817e324a3ec65ee55215f17f3
Cebolenkosi Shezi <cbshezi5@gmail.com>
2022-03-01 22:53:00 +0200

    Update students.sql

--- Database/students.sql
+++ Database/students.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 01, 2022 at 12:42 PM
--- Server version: 10.1.33-MariaDB
--- PHP Version: 7.2.6
+-- Generation Time: Mar 01, 2022 at 09:52 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.3.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `students`
+-- Database: `orientation_db_schema`
 --
 
 -- --------------------------------------------------------
@@ -29,9 +28,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `id` int(11) NOT NULL
+  `id` int(4) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -41,22 +40,14 @@ CREATE TABLE `admin` (
 --
 
 CREATE TABLE `student` (
-  `id` bigint(13) NOT NULL,
+  `id` int(4) NOT NULL,
   `studNum` int(9) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `surname` varchar(200) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `password` varchar(100) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(60) NOT NULL,
   `isVerified` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `student`
---
-
-INSERT INTO `student` (`id`, `studNum`, `name`, `surname`, `email`, `password`, `isVerified`) VALUES
-(2, 221145478, 'Nteli', 'nkuna', 'nteli@gmail.com', '25896', 0),
-(3, 147845478, 'mbuyelo', 'mabunda', 'mbuyelo@gmail.com', '86459', 0);
 
 --
 -- Indexes for dumped tables
@@ -66,13 +57,16 @@ INSERT INTO `student` (`id`, `studNum`, `name`, `surname`, `email`, `password`, 
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- Indexes for table `student`
 --
 ALTER TABLE `student`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `studNum` (`studNum`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -82,13 +76,13 @@ ALTER TABLE `student`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `id` bigint(13) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
