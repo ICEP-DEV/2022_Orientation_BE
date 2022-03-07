f52a364955990da8c050125635681bfdf1e2708e
Cheyeza <94190190+Cheyeza@users.noreply.github.com>
2022-03-07 11:28:06 +0200

    Update Stats.js

--- contexts/Stats.js
+++ contexts/Stats.js
@@ -53,4 +53,46 @@ Router.get('/', (req, res, next) => {
 
 });
 
+Router.put('/', (req, res) => {
+
+    let field;
+    let updateNum = 0;
+  
+    if (Object.keys(req.body).length == 0) {
+        res.send({
+            error: true,
+            code: "C001_PUT",
+            message: "body parameters were not found"
+        });
+        return
+    }
+
+    if (req.body.field) {
+        mariadb.query(`UPDATE stats SET  ${req.body.field} = ${req.body.updateNum} WHERE id = 1`,(err, rows,fields) => {
+            if (!err) {
+                res.send({
+                    error: false,
+                    data: rows //`Column with the name: ${req.body.field} has been updated.`
+                })
+                return
+            } else {
+                res.send({
+                    error: true,
+                    message: err,
+                    code: "C001_SQL_PUT"
+                })
+                return
+            }
+        });
+    } else {
+                res.send({
+                    error: true,
+                    message: "One or many of the required body arguements is missing",
+                    code: "C002_PUT"
+                })
+                return
+            }
+      
+})
+
 module.exports = Router;
