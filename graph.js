c02133713855f9cb77ca94ee0fb55c86610ecd9b
CebolenkosiShezi <cbshezi5@gmail.com>
2022-03-14 14:21:03 +0200

    restructuring

--- graph.js
+++ graph.js
@@ -8,23 +8,21 @@ const socket = require('socket.io')
 const connection = require("./connection")
 //Context file object
 //Student
-const TestQuery_cnxt = require('./contexts/TestQuery')
-const Registration_cnxt = require('./contexts/Registration')
-const Login_cnxt = require("./contexts/Login")
-const Student_cnxt = require("./contexts/Students")
-const Forgotten_cnxt = require("./contexts/Forgotten")
+const TestQuery_cnxt = require('./contexts/Common/TestQuery')
+const Registration_cnxt = require('./contexts/Authentication/Registration')
+const Login_cnxt = require("./contexts/Authentication/Login")
+const Student_cnxt = require("./contexts/Common/Students")
+const Forgotten_cnxt = require("./contexts/Authentication/Forgotten")
     //Admin
-const RegistrationAdm_cnxt = require('./contexts/Register_Adm')
-const LoginAdm_cnxt = require("./contexts/Login_Adm")
+const RegistrationAdm_cnxt = require('./contexts/Authentication/Register_Adm')
+const LoginAdm_cnxt = require("./contexts/Authentication/Login_Adm")
 
 //common
-const Stats_cnxt = require('./contexts/Stats')
-const Track_cnxt = require('./contexts/Tracking')
+const Stats_cnxt = require('./contexts/Statistics/Stats')
+const Track_cnxt = require('./contexts/Tracking/Tracking')
 
 
 app.use(bodyParser.json());
-
-
 
 app.use(function(req, res, next) {
     //Header allowences of METHODS
@@ -36,19 +34,19 @@ app.use(function(req, res, next) {
 });
 
 //context channelling Student
-app.use('/TestQuery', TestQuery_cnxt);
-app.use('/Registration', Registration_cnxt);
-app.use('/Student', Student_cnxt);
-app.use('/Login', Login_cnxt);
-app.use('/Forgotten',Forgotten_cnxt)
+app.use('/Test/TestQuery', TestQuery_cnxt);
+app.use('/Auth/Registration', Registration_cnxt);
+app.use('/Auth/Student', Student_cnxt);
+app.use('/Auth/Login', Login_cnxt);
+app.use('/Auth/Forgotten',Forgotten_cnxt)
 
 //context channelling Admin
-app.use('/Registration_Admin', RegistrationAdm_cnxt);
-app.use('/Login_Admin', LoginAdm_cnxt);
+app.use('/Auth/Registration_Admin', RegistrationAdm_cnxt);
+app.use('/Auth/Login_Admin', LoginAdm_cnxt);
 
 //context to common entities
-app.use('/Stats', Stats_cnxt);
-app.use('/Tracking', Track_cnxt)
+app.use('/Stat/Stats', Stats_cnxt);
+app.use('/Track/Tracking', Track_cnxt)
 
 const PORT = 6900
 var server = app.listen(PORT, (e) => {
@@ -60,6 +58,7 @@ var server = app.listen(PORT, (e) => {
 
 });
 
+localhost:6900/Track/Tracking
 
 
 connectionCount = 0
