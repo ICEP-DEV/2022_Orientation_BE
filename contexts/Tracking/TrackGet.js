0be2afee2c31aa3c4cd9bb503af0999d6a06ad74
CebolenkosiShezi <cbshezi5@gmail.com>
2022-04-08 15:06:02 +0200

    TrackDatetime

--- contexts/Tracking/TrackGet.js
+++ contexts/Tracking/TrackGet.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 Router.post('/', (req, res, next) => {
 
 
-    let query = "SELECT student.firstname, student.lastname, student.email, student.studNum, tracking.activity, tracking.datetime" +
+    let query = "SELECT tracking.id, student.firstname, student.lastname, student.email, student.studNum, tracking.activity, date_format(datetime, '%d/%m/%Y %h:%i %p') as datetime" +
         " FROM tracking, student" +
         " WHERE tracking.user_id = student.id"
 
