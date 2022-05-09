2578c280ae847b32e06713a318427cd0f8284124
CebolenkosiShezi <cbshezi5@gmail.com>
2022-05-09 09:06:54 +0200

    localhost

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const HOSTNAME = "ec2-3-80-224-126.compute-1.amazonaws.com"
+const HOSTNAME = "localhost"
+//localhost
+//ec2-3-80-224-126.compute-1.amazonaws.com"
 const express = require('express');
 const path = require('path');
 const app = express()
