9c3a17a6ecbe149c879b0b386b041a07f25fe801
Cebolenkosi Shezi <cbshezi5@gmail.com>
2022-05-18 13:25:46 +0200

    Update graph.js

--- graph.js
+++ graph.js
@@ -100,8 +100,7 @@ const server = app.listen(PORT, (e) => {
 //Backend Code
 var socketIO = socket(server,{
     cors: {
-      origin: ["https://master.dj4hobg2ew465.amplifyapp.com","mbuqssmxk3.us-east-1.awsapprunner.com","http://localhost:4200"],
-      credentials: false
+      origin: "*"
     }
 });
 var studentSessions = 0;
