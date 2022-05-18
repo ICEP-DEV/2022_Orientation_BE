ca5c255afb17f08045c451d5125d09a128819bc2
CebolenkosiShezi <cbshezi5@gmail.com>
2022-05-18 10:17:11 +0200

    connectToSheziInst

--- connection.js
+++ connection.js
@@ -1,11 +1,11 @@
-//Database establishment of connection
+//Database establishment of connection V05MplwnzsxSUPdKVjVd
 const mysql = require('mysql2');
 const prompt = require('prompt-sync')();
 
 const mariadb = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
+    host: 'db-orientation-inst.cvqpj5ith2h1.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'icep2020',
     database: 'orientation_db_schema'
 })
 
