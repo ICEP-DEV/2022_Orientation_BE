780ee46d0973d517955f54dddbab8cbd55d2c418
Cebolenkosi Shezi <cbshezi5@gmail.com>
2022-05-18 10:21:17 +0200

    Update connection.js

--- connection.js
+++ connection.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 const prompt = require('prompt-sync')();
 
 const mariadb = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
+    host: 'db-orientation-inst.cvqpj5ith2h1.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'icep2020',
     database: 'orientation_db_schema'
 })
 mariadb.connect()
